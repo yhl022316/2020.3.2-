440d4f6c177d2b1f0ac3fa0ccf176f9fd163c674
yuighgh <1833218368@qq.com>
2020-03-02 13:24:23 +0800

    函数基础

---
+++ 函数基础.py
@@ -0,0 +1,19 @@
+yhl='杨昊霖'
+#形参
+# #把实参调入到形参
+def name(name):
+    # print("我是杨昊霖")
+    # print("你是谁?")
+    # print(yhl)
+    print(name)
+name('你管我是谁呢')#实参
+
+
+def sum_num(num,num1):
+
+    return num+num1
+a=sum_num(22,55)#调用函数必须要有变量a来接受函数  最后print(a)打印结果
+print(a)
+
+
+
