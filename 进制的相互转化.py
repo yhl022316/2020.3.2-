723a579e0b535fb806cd8fb455fb18566dbf86a7
yuighgh <1833218368@qq.com>
2020-03-06 12:33:59 +0800

    2020三月份第一周代码

--- 进制的相互转化.py
+++ 进制的相互转化.py
@@ -1,55 +1,55 @@
 from pythonds.basic.stack import Stack
-# def divide2(desNumber):
-#     s=Stack()
-#     while desNumber>0:
-#          rem = desNumber % 2#取余数
-#
-#          s.push(rem)#push()将一个新项放到栈的顶部
-#          desNumber=desNumber//2#  取整
-#
-#     binString=""
-#     while not s.isEmpty():#isEmpty()测试是否为空
-#          binString=binString+str(s.pop())#pop()从栈中删除顶部项
-#
-#     return binString
-# print(divide2(233))
-#
-# print(11 // 2)
-#
-#
-# def divide3(desNumber1,base):
-#     a="0123456789ABCDEF"
-#     s=Stack()
-#
-#     while desNumber1>0:
-#         b=desNumber1 % 2
-#         s.push(b)
-#         desNumber1=desNumber1//base
-#     c=""
-#     while not s.isEmpty():
-#         c=c+a[s.pop()]
-#
-#     return c
-#
-# print(divide3(20,16))
-# print(divide3(50,8))
-#
-#
-# def divideBase(desNumber,base):
-#     digits = '0123456789ABCDEF'
-#     s = Stack()
-#
-#     while desNumber > 0:
-#         rem = desNumber % base
-#         s.push(rem)
-#         desNumber = desNumber // base
-#
-#     binString = ""
-#     while not s.isEmpty():
-#         binString = binString + digits[s.pop()]
-#
-#     return binString
-# print(divide3(20,16))
+def divide2(desNumber):
+    s=Stack()
+    while desNumber>0:
+         rem = desNumber % 2#取余数
+
+         s.push(rem)#push()将一个新项放到栈的顶部
+         desNumber=desNumber//2#  取整
+
+    binString=""
+    while not s.isEmpty():#isEmpty()测试是否为空
+         binString=binString+str(s.pop())#pop()从栈中删除顶部项
+
+    return binString
+print(divide2(769))
+
+print(11 // 2)
+
+
+def divide3(desNumber1,base):
+    a="0123456789ABCDEF"
+    s=Stack()
+
+    while desNumber1>0:
+        b=desNumber1 % 2
+        s.push(b)
+        desNumber1=desNumber1//base
+    c=""
+    while not s.isEmpty():
+        c=c+a[s.pop()]
+
+    return c
+
+print(divide3(1453,16))
+print(divide3(769,8))
+
+
+def divideBase(desNumber,base):
+    digits = '0123456789ABCDEF'
+    s = Stack()
+
+    while desNumber > 0:
+        rem = desNumber % base
+        s.push(rem)
+        desNumber = desNumber // base
+
+    binString = ""
+    while not s.isEmpty():
+        binString = binString + digits[s.pop()]
+
+    return binString
+print(divide3(769,16))
 # print(divide3(50,8))
 
 
@@ -62,38 +62,38 @@ def divide2(desNumber):
 
     binString=""
     while not s.isEmpty():#栈不是空的状态
-         binString=binString+str(s.pop)#取出来 pop()
+         binString=binString+str(s.pop())#取出来 pop()
 
     return binString
 print(divide2(233))
 
 
-# def six(desnumber2,base):
-#     a='0123456789ABCDEF'
-#     s=Stack()
-#     while desnumber2>0:
-#         h=desnumber2 % base
-#         s.push(h)
-#         desnumber2=desnumber2 // base
-#
-#     bin=""
-#     while not s.isEmpty():
-#         bin=bin+a[s.pop()]
-#
-#         return bin
-# print(six(5689,16))
+def six(desnumber2,base):
+    a='0123456789ABCDEF'
+    s=Stack()
+    while desnumber2>0:
+        h=desnumber2 % base
+        s.push(h)
+        desnumber2=desnumber2 // base
 
-# def divide2(desNumber):
-#     s=Stack()
-#     while desNumber>0:
-#          rem = desNumber % 2#取余数
-#
-#          s.push(rem)#push()将一个新项放到栈的顶部
-#          desNumber=desNumber//2#  取整
-#
-#     binString=""
-#     while not s.isEmpty():#isEmpty()测试是否为空
-#          binString=binString+str(s.pop())#pop()从栈中删除顶部项
-#
-#     return binString
-# print(divide2(233))
+    bin=""
+    while not s.isEmpty():
+        bin=bin+a[s.pop()]
+
+        return bin
+print(six(769,8))
+
+def divide2(desNumber):
+    s=Stack()
+    while desNumber>0:
+         rem = desNumber % 2#取余数
+
+         s.push(rem)#push()将一个新项放到栈的顶部
+         desNumber=desNumber//2#  取整
+
+    binString=""
+    while not s.isEmpty():#isEmpty()测试是否为空
+         binString=binString+str(s.pop())#pop()从栈中删除顶部项
+
+    return binString
+print(divide2(233))
